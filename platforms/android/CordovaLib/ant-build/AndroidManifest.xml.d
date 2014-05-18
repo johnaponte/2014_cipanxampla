e2af14369dab8e44cab33a5154d4f9afab7a71d6
johnaponte <japonte@udon.local>
2014-05-18 10:42:06 +0200

    Delete generated dirs

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/data/work/year2014/desarrollo/cordova/cipanxampla/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/data/work/year2014/desarrollo/cordova/cipanxampla/platforms/android/CordovaLib/AndroidManifest.xml \

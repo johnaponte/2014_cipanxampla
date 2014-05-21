10e8344e0a248d97dfe622f5f75da7d182e15490
johnaponte <japonte@udon.local>
2014-05-21 23:03:22 +0200

    Reestructuration of project

---
+++ platforms/android/assets/www/js/home.js
@@ -0,0 +1,11 @@
+//Anonym function that register the devicerady event
+(function() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+})();
+
+// device APIs are available
+function onDeviceReady() {
+	// Now safe to use device APIs
+	console.log("** Device Ready **");
+	navigator.splashscreen.hide();
+};

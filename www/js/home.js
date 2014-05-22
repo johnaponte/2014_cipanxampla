b336b1f4abb21b2a1fddab62b8d8cd38eb38b6f4
johnaponte <japonte@udon.local>
2014-05-22 12:25:46 +0200

    V2
    Full functional v2:
    Gallery (although a problem of slides on real galaxy phone) and basic
    alert function of the map (cordova notification not implemented)

--- www/js/home.js
+++ www/js/home.js
@@ -1,41 +1,14 @@
 //Anonym function that register the devicerady event
 (function() {
 	document.addEventListener("deviceready", onDeviceReady, false);
-//	document.addEventListener('DOMContentLoaded', fotogallery, false);
 
 })();
-
-//function fotogallery() {
-//	console.log("Called fotogallery");
-//	var options = {}, instance = PhotoSwipe.attach(window.document.querySelectorAll('#Gallery a'), options);
-//
-//}
 
 // device APIs are available
 function onDeviceReady() {
 	// Now safe to use device APIs
 	console.log("** Device Ready **");
 	navigator.splashscreen.hide();
-//
-//	$(document).on("pageshow", "#galeria", function(e) {
-//		console.log("** On pageshow of #galeria");
-//		e.preventDefault();
-//		var currentPage = $(e.target), options = {
-//			allowUserZoom : false
-//		}, photoSwipeInstance = $("ul.gallery a", e.target).photoSwipe(options, currentPage.attr('id'));
-//		return true;
-//	});
-//
-//	$(document).on("pagehide", "#galeria", function(e) {
-//		console.log("** On pagehide of #galeria");
-//		var currentPage = $(e.target), photoSwipeInstance = PhotoSwipe.getInstance(currentPage.attr('id'));
-//
-//		if ( typeof photoSwipeInstance != "undefined" && photoSwipeInstance != null) {
-//			PhotoSwipe.detatch(photoSwipeInstance);
-//		}
-//
-//		return true;
-//	});
-
+	
 };
 
